4342d755669fa44ee2d960cab7c7b9abcefd247e
abhishek kumar <abhisingh2084@gmail.com>
2024-06-03 13:31:00 +0530

    card added with the help of partial view

--- PartialViewASPCore/Controllers/HomeController.cs
+++ PartialViewASPCore/Controllers/HomeController.cs
@@ -18,6 +18,17 @@ namespace PartialViewASPCore.Controllers
             return View();
         }
 
+        public IActionResult Products()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Web 1", Description = "Website 1", Price = 60000, Image = "~/Images/image1.png"},
+                new Product() { Id = 2, Name = "Web 2", Description = "Website 2", Price = 70000, Image = "~/Images/image2.png"},
+                new Product() { Id = 3, Name = "Web 3", Description = "Website 3", Price = 80000, Image = "~/Images/image3.png"}
+            };
+            return View(products);
+        }
+
         public IActionResult Privacy()
         {
             return View();
